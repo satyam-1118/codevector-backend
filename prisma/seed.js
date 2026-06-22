c7475c3e4818ff645d4a133637cebee37df275f1
Your Name <you@example.com>
2026-06-22 16:20:12 +0530

    Add cursor encoding utilities

--- prisma/seed.js
+++ prisma/seed.js
@@ -9,8 +9,17 @@ function getRandomCategory(categories) {
 }
 
 async function main() {
-    console.log("🌱 Seed started...");
-    const categories = [
+  console.log("🌱 Seed started...");
+
+  // Delete old data
+  await prisma.product.deleteMany();
+  console.log("🗑 Old products deleted...");
+
+  // Test values
+  const TOTAL_PRODUCTS = 200000;
+  const BATCH_SIZE = 1000;
+
+  const categories = [
     "Electronics",
     "Fashion",
     "Books",
@@ -21,34 +30,35 @@ async function main() {
     "Grocery",
   ];
 
-  const products = [];
-  for (let i = 0; i < 10; i++) {
+  for (let batch = 0; batch < TOTAL_PRODUCTS / BATCH_SIZE; batch++) {
+    const products = [];
 
-    const product = {
-
+    for (let i = 0; i < BATCH_SIZE; i++) {
+      const product = {
         name: faker.commerce.productName(),
-
         category: getRandomCategory(categories),
+        price: Number(faker.commerce.price()),
+      };
 
-        price: Number(faker.commerce.price())
+      products.push(product);
+    }
 
-    };
+    const result = await prisma.product.createMany({
+      data: products,
+    });
 
-    products.push(product);
+    console.log(
+      `✅ Batch ${batch + 1} completed - Inserted ${result.count} products`
+    );
+  }
 
-}
-const result = await prisma.product.createMany({
-    data: products
-});
-
-console.log(result);
-
+  console.log("🎉 Seeding completed successfully!");
 }
 
 main()
-    .catch((error) => {
-        console.error(error);
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    });
+  .catch((error) => {
+    console.error("❌ Error:", error);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,10 +1,33 @@
-const getProducts = (req, res) => {
-    res.json({
-        success: true,
-        message: "Products API Working 🚀",
-    });
+const prisma = require("../config/prisma");
+
+const getProducts = async (req, res) => {
+    try {
+
+        const products = await prisma.product.findMany({
+            take: 20,
+            orderBy: {
+                updated_at: "desc"
+            }
+        });
+
+        res.status(200).json({
+            success: true,
+            count: products.length,
+            data: products
+        });
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+
+    }
 };
 
 module.exports = {
-    getProducts,
+    getProducts
 };
