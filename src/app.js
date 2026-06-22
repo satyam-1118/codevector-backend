8769ce7679049b2828a95b321ebd5cd4fb5530fa
Your Name <you@example.com>
2026-06-22 19:53:14 +0530

    Add root API route

--- src/app.js
+++ src/app.js
@@ -5,9 +5,19 @@ const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Home Route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "CodeVector Backend API is running 🚀",
+  });
+});
+
+// Product Routes
 app.use("/api/products", productRoutes);
 
 module.exports = app;
